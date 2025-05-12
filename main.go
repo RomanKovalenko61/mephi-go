4a6ed2e253887ea0007331e5de8109ec8a5b4433
Roman <astek14@mail.ru>
2025-05-12 10:23:48 +0300

    new own serveMux

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hello handler")
+}
 
 func main() {
-	fmt.Println("Mephi homework: finance app in go")
+	router := http.NewServeMux()
+	router.HandleFunc("/hello", hello)
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
+	fmt.Println("Server is listening on port 8081")
+	server.ListenAndServe()
 }
